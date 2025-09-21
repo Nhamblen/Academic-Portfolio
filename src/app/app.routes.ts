3bb4493a43022dbd6f16212467f375a940e61900
Noah <nhamblen21@gmail.com>
2025-09-21 11:23:13 -0500

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import page components
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent },   // default route
+  { path: 'about', component: AboutComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', component: NotFoundComponent } // wildcard (404)
+];
